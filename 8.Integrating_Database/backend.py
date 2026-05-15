5af2bde87d2606017c0963257b42bb8cf9898d6e
Manish Kumar <krmanish2302@gmail.com>
2026-05-15 09:09:56 +0530

    integrated sql database to store langraph states

---
+++ 8.Integrating_Database/backend.py
@@ -0,0 +1,44 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.1-8b-instant", temperature=0.7)
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+
+connection=sqlite3.connect(
+    database="chats.db", check_same_thread=False
+)  # why we are keeping check_same_thread=False, because sqlite3 keep things in only one thread and not in multiple threads at the same time
+# Checkpointer
+checkpointer = SqliteSaver(conn=connection)
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return all_threads
